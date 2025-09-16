b07b8201708a15254eac8f1eac0637b9722a9f3c
zysgusg <zysgusg@foxmail.com>
2025-09-16 17:20:17 +0800

    add start.sh

---
+++ start.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+DIR="$HOME/dotfiles"
+FILES=(
+	".bashrc"
+	".vimrc"
+	".gitconfig"
+)
+for file in "${FILES[@]}";do
+	src="$DIR/$file"
+	dest="$HOME/$file"
+	if [ -L "$dest" ]; then
+		echo "Symlink exists: $dest"
+	elif [ -e "$dest" ]; then
+		mv "$dest" "$dest.bak"
+		echo "Backed up $dest to $dest.bak"
+		ln -sf "$src" "$dest"
+		echo "Linked $src -> $dest"
+	else
+		ln -sf "$src" "$dest"
+		echo "Linked $src -> $dest"
+	fi
+done
